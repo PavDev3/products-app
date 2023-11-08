f59a9c7a0be25daea73f4478e917c665a2663efb
Pablo <pnunfe@gmail.com>
2023-11-08 17:45:48 +0100

    Add product details functionality to ProductDetailsComponent

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -8,17 +8,27 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  ngOnInit() {
-    const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routeParams.get('productId'));
-
-    this.product = products.find(product => product.id === productIdFromRoute);
- }
-  product: Product | undefined;
+  product: Product | undefined; // Aquí se declara una variable llamada 'product' que almacenará los detalles del producto.
 
   constructor(private route: ActivatedRoute) {
+    // Este es el constructor de la clase ProductDetailsComponent.
+    // El constructor se encarga de inyectar una dependencia llamada 'route' (es como darle un juguete especial al componente).
+  }
 
+  ngOnInit() {
+    // Este método se llama cuando el componente se inicializa.
+    // Es como preparar todo antes de mostrar la página.
 
+    const routeParams = this.route.snapshot.paramMap;
+    // Aquí obtienes información sobre la URL actual, como una dirección en un mapa.
+
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    // Esto toma un número de la URL (por ejemplo, el número de identificación del producto que deseas mostrar).
+
+    // Ahora, vamos a buscar el producto correcto en una lista de productos.
+    this.product = products.find(product => product.id === productIdFromRoute);
+    // Aquí, buscas en una lista de productos el que coincida con el número de identificación obtenido de la URL.
+  }
 }
-}
+
 
